1f738a5976c56afefb75579c02272922a6d8eea2
Zombie9904 <tonkla9904@gmail.com>
2021-11-15 17:42:13 +0700

    update

--- lib/WelcomeScreen.dart
+++ lib/WelcomeScreen.dart
@@ -13,91 +13,120 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(children: [
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            'MINEFUL',
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.w900,
-              color: Colors.pink,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/images/BG4.png'),
+                    fit: BoxFit.cover,
+                    colorFilter:
+                        ColorFilter.mode(Colors.white, BlendMode.darken))),
+          ), //พื้นหลัง
+          Column(children: [
+            SizedBox(
+              height: 1,
             ),
-          ),
-          Center(
-            child: Container(
-              height: 350,
-              width: 500,
-              child: Stack(children: [
-                Positioned(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage('assets/images/logo4.png'),
-                            fit: BoxFit.scaleDown)),
-                  ),
-                ),
-              ]),
+            SizedBox(
+              child: Container(
+                  height: 350,
+                  width: 600,
+                  child: Column(
+                    children: [
+                      AnimatedImage(),
+                    ],
+                  )),
             ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          Padding(
-            padding: EdgeInsets.all(25),
-            child: Column(children: [
-              SizedBox(
-                width: 400,
-                height: 55,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      primary: Color(0xFFff7722)),
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => SignIn()));
-                  },
-                  child: Text(
-                    "Sign In",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: 400,
-                height: 55,
-                child: ElevatedButton(
+            SizedBox(
+              height: 25,
+            ),
+            Padding(
+              padding: EdgeInsets.all(25),
+              child: Column(children: [
+                SizedBox(
+                  width: 400,
+                  height: 55,
+                  child: ElevatedButton(
                     style: ElevatedButton.styleFrom(
                         shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(15),
                         ),
-                        primary: Colors.grey[700]),
+                        primary: Colors.white),
                     onPressed: () {
                       Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => SignUp()));
+                          MaterialPageRoute(builder: (context) => SignIn()));
                     },
                     child: Text(
-                      "Sign Up",
+                      "Sign In",
                       style: TextStyle(
-                          color: Colors.white,
+                          color: Colors.black,
                           fontWeight: FontWeight.bold,
                           fontSize: 20),
-                    )),
-              ),
-            ]),
-          )
-        ]),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 400,
+                  height: 55,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                          primary: Colors.grey[700]),
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) => SignUp()));
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20),
+                      )),
+                ),
+              ]),
+            )
+          ]),
+        ],
       ),
+    );
+  }
+}
+
+class AnimatedImage extends StatefulWidget {
+  const AnimatedImage({Key? key}) : super(key: key);
+
+  @override
+  _AnimatedImageState createState() => _AnimatedImageState();
+}
+
+class _AnimatedImageState extends State<AnimatedImage>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+  )..repeat(reverse: true);
+  late Animation<Offset> _animation = Tween(
+    begin: Offset.zero,
+    end: Offset(0, 0.10),
+  ).animate(_controller);
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SlideTransition(
+      position: _animation,
+      child: Image.asset('assets/images/logo4.png'),
     );
   }
 }
