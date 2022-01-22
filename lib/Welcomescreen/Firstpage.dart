c910fb9ae3b4578d98402571447079b154bc604c
Zombie9904 <tonkla9904@gmail.com>
2022-01-22 22:16:55 +0700

    F1

--- lib/Welcomescreen/Firstpage.dart
+++ lib/Welcomescreen/Firstpage.dart
@@ -14,89 +14,133 @@ class _FirstpageState extends State<Firstpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.longestSide,
-            width: MediaQuery.of(context).size.shortestSide,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/BG6.png"),
-                  fit: BoxFit.cover),
-            ),
-          ),
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(10.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text('already have an account?', style: font),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => NavScreen()));
-                        },
-                        child: Text(
-                          "Login",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    'Welcome to Mineful',
-                    style: font2,
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text(
-                    'Create an account',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  F1('First name', Icons.person),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  F1('Last name', Icons.person_add),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  F1('Username', Icons.verified),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  F1('Email adress', Icons.email),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  F1('password (6+ characters)', Icons.password),
-                  SizedBox(
-                    height: 15,
-                  ),
-                ],
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.longestSide,
+              width: MediaQuery.of(context).size.shortestSide,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/BG6.png"),
+                    fit: BoxFit.cover),
               ),
             ),
-          ),
-        ],
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text('already have an account?', style: font),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => NavScreen()));
+                          },
+                          child: Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 13,
+                    ),
+                    Text(
+                      'Welcome to Mineful',
+                      style: font2,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text(
+                      'Create an account',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    F1('First name', Icons.person), //from Text filedF1.dart
+                    SizedBox(
+                      height: 15,
+                    ),
+                    F1('Last name', Icons.person_add), //from Text filedF1.dart
+                    SizedBox(
+                      height: 15,
+                    ),
+                    F1('Username', Icons.verified), //from Text filedF1.dart
+                    SizedBox(
+                      height: 15,
+                    ),
+                    F1('Email adress', Icons.email), //from Text filedF1.dart
+                    SizedBox(
+                      height: 15,
+                    ),
+                    F1('password (6+ characters)',
+                        Icons.password), //from Text filedF1.dart
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Elevationbutton1("Continue", Colors.black, Colors.white),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Elevationbutton1("Continue with Google",
+                        Colors.grey.shade300, Colors.black),
+                    SizedBox(
+                      height: 55,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Row(
+                        children: [
+                          Text(
+                            "By continuing you agree to Mindful",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12),
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Term("Terms & Conditions"),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "and",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(6.0),
+                      child: Term("Privacy Policy"),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/text_filed_F1.dart
+++ lib/widgets/text_filed_F1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_1/screen/nav_screen.dart';
 
 class F1 extends StatelessWidget {
   String hint;
@@ -30,6 +31,48 @@ class F1 extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Elevationbutton1 extends StatelessWidget {
+  late String text1;
+  late Color colors1;
+  late Color colors2;
+  Elevationbutton1(this.text1, this.colors1, this.colors2);
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(13),
+            ),
+            minimumSize: Size(320, 50),
+            primary: colors1,
+            onPrimary: colors2),
+        onPressed: () {},
+        child: Text(
+          text1,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class Term extends StatelessWidget {
+  String text2;
+  Term(this.text2);
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Text(
+        text2,
+        style: TextStyle(
+            color: Colors.blue, fontSize: 12, fontWeight: FontWeight.bold),
       ),
     );
   }
