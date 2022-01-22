467f17226a95934b3e111609b932a27dfd0615e9
Zombie9904 <tonkla9904@gmail.com>
2022-01-22 22:17:48 +0700

    F1

--- lib/widgets/text_filed_F1.dart
+++ lib/widgets/text_filed_F1.dart
@@ -1,5 +1,6 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
-import 'package:flutter_project_1/screen/nav_screen.dart';
 
 class F1 extends StatelessWidget {
   String hint;
